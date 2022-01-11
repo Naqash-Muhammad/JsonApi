aac6cb9be6535f1b2a361d86afdbaf985fa63d5d
Naqash-Muhammad <92191163+Naqash-Muhammad@users.noreply.github.com>
2022-01-11 10:57:04 -0700

    AddApi
    hi-api

---
+++ StartApi.js
@@ -0,0 +1,37 @@
+const Api = [
+    {
+        id : "1",
+        Name : "Naqash",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+    {
+        id : "2",
+        Name : "ahead",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "3",
+        Name : "banti",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "4",
+        Name : "Sani",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    {
+        id : "5",
+        Name : "Taha",
+        OS :"Muhammad Ali",
+        study : "BsCs",
+    },
+
+    console.log(Api)
+]
